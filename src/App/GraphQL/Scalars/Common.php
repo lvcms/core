a68fece59af67edc35e7abf21aaf468480603b28
bigrocs <bigrocs@qq.com>
2018-04-17 19:41:09 +0800

    增加公共标量央视

---
+++ src/App/GraphQL/Scalars/Common.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Laracore\Core\App\GraphQL\Scalars;
+
+use Folklore\GraphQL\Support\Contracts\TypeConvertible;
+use GraphQL\Type\Definition\ScalarType;
+
+class Common extends ScalarType implements TypeConvertible
+{
+    /**
+     * @var string
+     */
+    public $name = 'Common';
+
+    /**
+     * @var string
+     */
+    public $description = '';
+
+    /**
+     * @param mixed $value
+     * @return mixed
+     */
+    public function serialize($value)
+    {
+        return $value;
+    }
+
+    /**
+     * @param mixed $value
+     * @return string
+     */
+    public function parseValue($value)
+    {
+        return $value;
+    }
+
+    /**
+     * @param $ast
+     * @return null|string
+     */
+    public function parseLiteral($ast)
+    {
+        return null;
+    }
+
+    /**
+     * @return Main
+     */
+    public function toType()
+    {
+        return new static();
+    }
+}
--- src/Config/graphql.php
+++ src/Config/graphql.php
@@ -4,17 +4,20 @@ return [
         'default' => [
             'query' => [
                 'users' => Laracore\Core\App\GraphQL\Query\UserQuery::class,
+
                 'vueRouter' => Laracore\Core\App\GraphQL\Query\VueRouterQuery::class,
             ],
             'mutation' => [
-                'updateUserPassword' =>  Laracore\Core\App\GraphQL\Mutation\UpdateUserPasswordMutation::class,
-                'updateUserEmail' =>  Laracore\Core\App\GraphQL\Mutation\UpdateUserEmailMutation::class,
+                // 'updateUserPassword' =>  Laracore\Core\App\GraphQL\Mutation\UpdateUserPasswordMutation::class,
+                // 'updateUserEmail' =>  Laracore\Core\App\GraphQL\Mutation\UpdateUserEmailMutation::class,
             ]
         ]
     ],
 
     'types' => [
         'User' =>  Laracore\Core\App\GraphQL\Type\UserType::class,
+
+        'CommonScalar' =>  Laracore\Core\App\GraphQL\Scalars\Common::class,
         'VueRouter' =>  Laracore\Core\App\GraphQL\Type\VueRouterType::class,
     ],
 ];
