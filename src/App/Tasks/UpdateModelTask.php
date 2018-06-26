0c54f9dc633f3afe8be3e0363fa68563dd96c26e
bigrocs <bigrocs@qq.com>
2018-06-26 20:30:17 +0800

    修复错误

--- src/App/Tasks/UpdateModelTask.php
+++ src/App/Tasks/UpdateModelTask.php
@@ -2,10 +2,15 @@
 
 namespace Laracore\Core\App\Tasks;
 
+use Laracore\Core\Framework\Contracts\Frontend\Model;
+
 class UpdateModelTask
 {
     public function run($args)
     {
-        dd($args);
+      return app()->make(Model::class)
+              ->setPackage($args['package'])
+              ->setModel($args['model'])
+              ->update($args['value']);
     }
 }
