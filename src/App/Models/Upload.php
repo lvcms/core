6c1ec7f1e76f7a8a9aed6644e7f2c9d58fb11dec
bigrocs <bigrocs@qq.com>
2018-06-27 18:55:33 +0800

    增加 upload 模型

---
+++ src/App/Models/Upload.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Laracore\Core\App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Upload.
+ */
+class Upload extends Model
+{
+    public $table = 'core_uploads';
+
+}
