942e7ac7f0532c9e143cd7ba488b2b86ea393449
bigrocs <bigrocs@qq.com>
2018-07-26 14:31:46 +0800

    渲染组件 renderComponent

--- src/Framework/Frontend/Model/Config.php
+++ src/Framework/Frontend/Model/Config.php
@@ -24,6 +24,8 @@ class Config
         $this->config = collect(config($this->package.'.model.'.$this->modelName));
         $this->config['layout'] = $this->layout();
         $this->config['auth'] = $this->auth();
+        $this->handlerItem();
+
     }
     /** 
      * 获取全部配置
@@ -45,7 +47,38 @@ class Config
     {
         return $this->layoutDefalutConfig($this->config->get('layout'));
     }
-
+    /**
+     * 处理 item 渲染 handlerItem
+     */
+    public function handlerItem()
+    {
+        $item = $this->config->get('item');
+        // 循环所有 item 项目
+        foreach ($item as &$val) {
+            // 循环每个项目下面的组件
+            foreach ($val['item'] as &$component) {
+                $component = $this->renderComponent($component);
+            }
+        }
+        $this->config['item'] = $item;
+    }
+    /**
+     * 渲染组件 renderComponent
+     */
+    private function renderComponent($component)
+    {
+        switch ($component['component']) {
+            case 'upload':
+                $configUpload = config('core.upload');
+                // 根据上传文件样式 选择图片或者文件的上传格式限制
+                $configUpload['format'] = $configUpload['format'][$component['fileType']];
+                $component = array_merge($configUpload, $component);
+                break;
+            default:
+                break;
+        }
+        return $component;
+    }
     /** 
      * 增加 layout 默认配置
      * 自动加载 layout col 配置
